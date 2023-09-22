ce94cce62c3a10dd7df027fcd8028ec0af7420dd
Tudor Butufei <tudor.butufei@gmail.com>
2023-09-22 14:23:06 +0300

    homepage redirects to course dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,5 @@
-import { Card, Grid, Text, Title } from "@tremor/react";
-import prisma from './_lib/prisma';
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const posts = await prisma.post.findMany();
-
-  return (
-    <main className="p-5 md:p-24">
-      <Grid numItemsMd={2} numItemsLg={3} className="gap-6 mt-6">
-        {posts.map(post => (
-          <Card className="dark:hover:bg-slate-800 hover:bg-slate-50 active:bg-slate-100 cursor-pointer" key={post.id}>
-            <Title>{post.title}</Title>
-            <Text>{post.content}</Text>
-          </Card>
-        ))}
-      </Grid>
-    </main>
-  )
+  redirect("/courses");
 }
