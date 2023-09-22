b033535ec0ffa27fa2ecd1f27b3ca59f84ef5fa2
Tudor Butufei <tudor.butufei@gmail.com>
2023-09-22 14:22:01 +0300

    work only for light theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@tremor/**/*.{js,ts,jsx,tsx}', // Tremor module
   ],
-  // darkMode: 'class',
+  darkMode: 'class',
   theme: {
     transparent: "transparent",
     current: "currentColor",
