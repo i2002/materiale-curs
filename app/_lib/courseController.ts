96de14336917e6f9d9eea91298371c807624d32b
Tudor Butufei <tudor.butufei@gmail.com>
2023-09-25 21:52:54 +0300

    database controller for courses and resources

---
+++ app/_lib/courseController.ts
@@ -0,0 +1,80 @@
+import { Resource } from "@prisma/client";
+import prisma from "./prisma";
+
+export async function getCourses() {
+  return await prisma.course.findMany({
+    orderBy: [
+      { year: "asc" },
+      { semester: "asc" },
+      { name: "asc" }
+    ]
+  });
+}
+
+export async function getCourse(slug: string) {
+  return await prisma.course.findUnique({
+    where: { slug: slug }
+  });
+}
+
+export async function getResource(resId: string | undefined, courseSlug: string) {
+  if (!resId) {
+    let course = await getCourse(courseSlug);
+    if (!course) {
+      return null;
+    }
+
+    resId = course.rootFolderId;
+  }
+
+  return await prisma.resource.findUnique({
+    where: { id: resId },
+    include: {
+      children: {
+        orderBy: [
+          { type: "desc" },
+          { name: "asc" }
+        ]
+      }
+    }
+  });
+}
+
+type PathSegment = {
+  name: string;
+  id: string;
+}
+
+export type ResourcePath = Array<PathSegment>;
+
+/**
+ * Get the path (root resource, route segments and leaf resource) of a specified resource.
+ * @param resId the id of the resource
+ * @returns 
+ */
+export async function getResourcePath(res: Resource): Promise<ResourcePath> {
+  // query the segments  
+  let segments: Array<PathSegment> = [{
+    name: res.name,
+    id: res.id
+  }];
+
+  try {
+    while(res.parentId) {
+      res = await prisma.resource.findUniqueOrThrow({ where: { id: res.parentId } });
+  
+      segments.push({
+        name: res.name,
+        id: res.id
+      });
+    }
+  } catch {}
+
+  // build the result object
+  let path = [];
+  for (let i = segments.length - 2; i >= 0; i--) {
+    path.push(segments[i]);
+  }
+
+  return path;
+}
