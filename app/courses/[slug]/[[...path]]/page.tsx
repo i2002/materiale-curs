7a7c3738ed47cf6caf03b423da9a7132f11cf84c
Tudor Butufei <tudor.butufei@gmail.com>
2023-09-28 00:16:35 +0300

    show resource metadata

--- app/courses/[slug]/[[...path]]/page.tsx
+++ app/courses/[slug]/[[...path]]/page.tsx
@@ -1,3 +1,26 @@
+import { getResource } from "@/app/_lib/courseController";
+import { Metadata, ResolvingMetadata } from "next";
+
+interface Props {
+  params: { slug: string, path: string[] };
+  searchParams: { [key: string]: string | string[] | undefined };
+}
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  let res = await getResource(params.slug, params.path);
+
+  if (!res || res.parentId == null) {
+    return {};
+  }
+
+  return {
+    title: res.name
+  };
+}
+
 export default function Page() {
-  return "";
+  return null;
 }
