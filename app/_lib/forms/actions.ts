25247e5a2d09ca22322f6b386903f13d883b7511
Tudor Butufei <tudor.butufei@gmail.com>
2023-10-06 15:39:50 +0300

    unique list name

--- app/_lib/forms/actions.ts
+++ app/_lib/forms/actions.ts
@@ -3,13 +3,14 @@
 import { revalidatePath } from "next/cache";
 import { studentListFormSchema } from "./form_schemas";
 import prisma from "../prisma";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 export async function createStudentList(values: {[key: string]: string}) {
   let data;
   try {
     data = await studentListFormSchema.validate(values);
   } catch (err) {
-    console.log(err); //FIXME error logging
+    console.log(err); // FIXME: error logging
     return {
       error: "Eroare la validare date"
     };
@@ -27,11 +28,16 @@ export async function createStudentList(values: {[key: string]: string}) {
           }))
         }
       }
-    })
+    });
   } catch (e) {
-    console.error(e);
+    console.error(e); // FIXME: error logging
+
+    let message = "Eroare la creare listă. Încercați din nou.";
+    if (e instanceof PrismaClientKnownRequestError && e.code == "P2002") {
+      message = "Nume listă existent.";
+    }
     return {
-      error: "Eroare la adăugare listă"
+      error: message
     };
   }
 
