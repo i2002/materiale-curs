c69ac507dcd4fb06bcc095374eab0681d9b8c52e
Tudor Butufei <tudor.butufei@gmail.com>
2023-10-08 16:08:28 +0300

    admin list courses

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -2,6 +2,7 @@ import { Button } from "@tremor/react";
 import ListTable from "@/components/admin/dataView/ListTable";
 import Link from "next/link";
 import { TrashIcon } from "@heroicons/react/20/solid";
+import { getCourses } from "@/lib/controllers/courseController";
 
 
 const cols = [
@@ -12,19 +13,22 @@ const cols = [
 ];
 
 const getItems = async () => {
-  await new Promise(r => setTimeout(r, 2000));
-  return [
-    {
-      data: ["Autocad", "1", "2", "CTI"],
-      actions: [ { name: "Delete", icon: <TrashIcon></TrashIcon>, href: "#" } ],
-      href: "/admin/courses/edit/1"
-    },
-    {
-      data: ["FRC", "2", "1", "CTI"],
-      actions: [ { name: "Delete", icon: <TrashIcon></TrashIcon>, href: "#" } ],
-      href: "/admin/courses/edit/2"
-    }
-  ];
+  let courses = await getCourses();
+  
+  return courses.map(course => ({
+    data: [
+      course.name,
+      String(course.year),
+      String(course.semester),
+      course.specialization
+    ],
+    actions: [{
+      name: "Delete",
+      icon: <TrashIcon></TrashIcon>,
+      href: `/admin/courses/delete/${course.id}`
+    }],
+    href: `/admin/courses/view/${course.id}`
+  }));
 }
 
 export default function CourseDashboard() {
@@ -32,7 +36,7 @@ export default function CourseDashboard() {
     <div className="px-6 py-4">
       <div className="flex justify-end my-3">
         <Link href="/admin/courses/create">
-          <Button color="teal">Add course</Button>
+          <Button color="teal">Curs nou</Button>
         </Link>
       </div>
       <ListTable columns={cols} getItems={getItems}></ListTable>
