9832bf4bdf0d5cd34012da29786036692b5478b0
Tudor Butufei <tudor.butufei@gmail.com>
2023-10-14 17:25:13 +0300

    course student list component

--- app/admin/courses/view/[id]/@students/default.tsx
+++ app/admin/courses/view/[id]/@students/default.tsx
@@ -1,3 +1,19 @@
-export default function Page() {
-  return "Studenți";
+import CourseStudentListsForm from "@/components/admin/forms/CourseStudentListsForm";
+import { getCourseLinkedLists } from "@/lib/controllers/courseController";
+import { getStudentLists } from "@/lib/controllers/studentListController";
+
+
+type Props = {
+  params: { id: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export default async function Page({ params }: Props) {
+  let courseId = parseInt(params.id);
+  const lists = await getStudentLists();
+  const linkedLists = await getCourseLinkedLists(courseId) ?? [];
+
+  return (
+    <CourseStudentListsForm courseId={courseId} initialData={linkedLists} lists={lists}></CourseStudentListsForm>
+  );
 }
