fd4ad84b5d17f745a4666cce74beceff258c9460
Tudor Butufei <tudor.butufei@gmail.com>
2023-10-16 01:28:34 +0300

    add folder view component to view course page

--- app/admin/courses/view/[id]/@tabs/files/page.tsx
+++ app/admin/courses/view/[id]/@tabs/files/page.tsx
@@ -1,3 +1,13 @@
-export default function Page() {
-  return "Fișiere";
+import FolderView from "@/components/admin/dataView/FolderView";
+
+type Props = {
+  params: { id: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export default function Page({ params }: Props) {
+  const courseId = parseInt(params.id);
+  return (
+    <FolderView courseId={courseId}></FolderView>
+  );
 }
