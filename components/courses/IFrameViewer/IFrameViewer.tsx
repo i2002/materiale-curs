adeebdf3ca3e4eb3418b0e2b36dbe101d8585500
Tudor Butufei <tudor.butufei@gmail.com>
2023-10-30 05:06:43 +0200

    iframeviewer code cleanup

--- components/courses/IFrameViewer/IFrameViewer.tsx
+++ components/courses/IFrameViewer/IFrameViewer.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { useEffect, useRef } from "react";
 import ResourceViewerHeader from "../ResourceViewer/ResourceViewerHeader";
 
 interface Props {
@@ -11,11 +10,7 @@ interface Props {
 export default function IFrameViewer({ name, resUrl }: Props) {
   return (
     <ResourceViewerHeader name={name}>
-      <iframe src={resUrl} className="w-full h-full absolute z-10" sandbox="allow-scripts" onLoad={(e) => {
-        console.log("AAA");
-        console.log(e);
-      }}></iframe>
-      <span className="absolute top-0 w-full h-full text-white">H</span>
+      <iframe src={resUrl} className="w-full h-full" sandbox=""></iframe>
     </ResourceViewerHeader>
   );
 }
